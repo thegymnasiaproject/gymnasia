90de6ad71ae0a40f31fe9660675e3181aa7e57e8
alexbaumgertner <alex.baumgertner@gmail.com>
2023-07-08 17:07:43 +0300

    Add habit builder scaffold

--- src/app/turtle-pace/page.tsx
+++ src/app/turtle-pace/page.tsx
@@ -1,151 +1,143 @@
-'use client'
+'use client';
 
-import React from 'react'
+import React, { useState } from 'react';
 import {
-  Button,
-  Checkbox,
-  Col,
-  Form,
-  Input,
-  InputNumber,
-  message,
-  Row,
-  Space,
-  Tag,
-  Typography,
-  Table,
-} from 'antd'
+    Button,
+    Checkbox,
+    Col,
+    Form,
+    Input,
+    InputNumber,
+    message,
+    Row,
+    Space,
+    Table,
+} from 'antd';
 
-import type { CheckboxChangeEvent } from 'antd/es/checkbox'
-import type { ColumnsType } from 'antd/es/table'
+import type { ColumnsType } from 'antd/es/table';
 
 interface DataType {
-  key: string;
-  isDone: boolean;
-  day: number;
-  minutes: number;
+    key: string;
+    isDone: boolean;
+    day: number;
+    minutes: number;
 }
 
 const columns: ColumnsType<DataType> = [
-  {
-    title: 'Я молодец',
-    dataIndex: 'isDone',
-    key: 'isDone',
-    render: (isDone) => <> <Checkbox checked={isDone}></Checkbox></>,
-  },
-  {
-    title: 'День',
-    dataIndex: 'day',
-    key: 'day',
-  },
-  {
-    title: 'Сколько минут практиковал(а)',
-    dataIndex: 'minutes',
-    key: 'minutes',
-  },
-  {
-    title: 'Процент',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (text) => <a>{text}</a>,
-  },
-]
-
-const data: DataType[] = [
-  {
-    key: '1',
-    isDone: true,
-    day: 1,
-    minutes: 3,
-  },
-  {
-    key: '2',
-    isDone: true,
-    day: 2,
-    minutes: 3,
-  },
-  {
-    key: '3',
-    isDone: false,
-    day: 3,
-    minutes: 3,
-  },
-  {
-    key: '4',
-    isDone: false,
-    day: 4,
-    minutes: 3,
-  },
-]
-
-const onChange = (e: CheckboxChangeEvent) => {
-  console.log(`checked = ${e.target.checked}`)
-}
+    {
+        title: 'Я молодец',
+        dataIndex: 'isDone',
+        key: 'isDone',
+        render: (isDone) => <> <Checkbox checked={isDone}></Checkbox></>,
+    },
+    {
+        title: 'День',
+        dataIndex: 'day',
+        key: 'day',
+    },
+    {
+        title: 'Сколько минут практиковал(а)',
+        dataIndex: 'minutes',
+        key: 'minutes',
+    },
+    {
+        title: 'Процент',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: (text) => <a>{text}</a>,
+    },
+];
 
 const Page = () => {
-  const [form] = Form.useForm()
+    const [data, setData] = useState<DataType[]>([]);
 
-  const onFinish = () => {
-    message.success('Submit success!')
-  }
+    const [form] = Form.useForm();
 
-  const onFinishFailed = () => {
-    message.error('Submit failed!')
-  }
+    const onFinish = (formData: { habitName: string, percentPerDay: number }) => {
+        console.log('data: ', formData); // eslint-disable-line
 
-  const onChange = (value: number | null) => {
-    console.log('changed', value)
-  }
 
-  return (
-    <div>
-      <h2>HABIT BUILDER</h2>
+        const totalDays = 66; // Total days to maintain the habit
+        const calculatedData: DataType[] = [];
 
-      <Row>
-        <Col span={6}>
-          <Form
-            form={form}
-            layout="vertical"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item
-              name="habit"
-              label="Привычка для практики"
-              rules={[
-                { required: true },
-                { type: 'string', warningOnly: true },
-                { type: 'string', min: 6 },
-              ]}
-            >
-              <Input placeholder="input placeholder" />
-            </Form.Item>
-            <Form.Item
-              name="hours"
-              label="На сколько процентов в день увеличивать время практики?"
-              rules={[
-                { required: true },
-                { type: 'number' },
-              ]}
-            >
-              <InputNumber min={1} max={10} defaultValue={5} onChange={onChange} />
-            </Form.Item>
-            <Form.Item>
-              <Space>
-                <Button type="primary" htmlType="submit">
-                  Добавить
-                </Button>
-              </Space>
-            </Form.Item>
-          </Form>
-        </Col>
-        <Col span={18}>
-          <Table columns={columns} dataSource={data} />
-        </Col>
-      </Row>
-    </div>
-  )
-}
+        for (let i = 0; i < totalDays; i++) {
+            calculatedData.push({
+                key: Math.random() + '',
+                isDone: false,
+                day: i + 1,
+                minutes: 10, // TODO
+            });
+        }
 
-export default Page
+        console.log('calculatedData: ', calculatedData); // eslint-disable-line
+
+        setData(calculatedData);
+
+        message.success('Submit success!');
+    };
+
+    const onFinishFailed = (formError: {}) => {
+        console.log('error: ', formError); // eslint-disable-line
+        message.error('Submit failed!');
+    };
+
+    const onChange = (value: number | null) => {
+        console.log('changed', value);
+    };
+
+    return (
+            <div>
+                <h2>HABIT BUILDER</h2>
+
+                <Row>
+                    <Col span={24}>
+                        <Form
+                                form={form}
+                                layout="vertical"
+                                onFinish={onFinish}
+                                onFinishFailed={onFinishFailed}
+                                autoComplete="off"
+                                initialValues={data}
+                        >
+                            <Form.Item
+                                    name="habitName"
+                                    label="Привычка для практики"
+                                    rules={[
+                                        { required: true },
+                                        { type: 'string', warningOnly: true },
+                                        { type: 'string', min: 2 },
+                                    ]}
+                            >
+                                <Input placeholder="input placeholder" />
+                            </Form.Item>
+                            <Form.Item
+                                    name="percentPerDay"
+                                    label="На сколько процентов в день увеличивать время практики?"
+                                    rules={[
+                                        { required: true },
+                                        { type: 'number' },
+                                    ]}
+                            >
+                                <InputNumber min={1} max={10} onChange={onChange} />
+                            </Form.Item>
+                            <Form.Item>
+                                <Space>
+                                    <Button type="primary" htmlType="submit">
+                                        Добавить
+                                    </Button>
+                                </Space>
+                            </Form.Item>
+                        </Form>
+                    </Col>
+
+                </Row>
+                <Row>
+                    <Col span={24}>
+                        <Table columns={columns} dataSource={data} />
+                    </Col>
+                </Row>
+            </div>
+    );
+};
+
+export default Page;
